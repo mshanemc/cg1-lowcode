7efba6288913b3ee4418df11920189d66f527986
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-08-14 17:33:50 -0500

    username/password for mobile

--- orgInit.sh
+++ orgInit.sh
@@ -1,5 +1,6 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 1 -s
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s -o df18.booth -u cg1
 sfdx shane:github:package:install --githubuser mshanemc --repo volvo-base
+sfdx shane:user:password:set -g User -l User -p DF18*cg1
 sfdx force:user:permset:assign -n VehiclesPerms
 sfdx force:source:push
 sfdx force:org:open
