48202dc4fe7aa9f21036e8c8f079aa7d734715bb
Shane McLaughlin <shane.mclaughlin@salesforce.com>
2018-08-16 16:32:48 -0500

    data setup calls + build tasks

--- orgInit.sh
+++ orgInit.sh
@@ -1,6 +1,7 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s -o df18.booth -u cg1
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s -o df18.booth -u cg1_
 sfdx shane:github:package:install --githubuser mshanemc --repo volvo-base
 sfdx shane:user:password:set -g User -l User -p DF18*cg1
 sfdx force:user:permset:assign -n VehiclesPerms
 sfdx force:source:push
+sfdx force:apex:execute -f scripts/init.cls
 sfdx force:org:open
