a7ebe9de6d2ac2e86a124726f638c9808e66a6de
mshanemc <shane.mclaughlin@salesforce.com>
2019-01-09 09:58:54 -0600

    no -u allowed on shane:org:create

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s -o df18.booth -u cg1_
+sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s -o df18.booth --userprefix cg1_
 sfdx shane:github:package:install --githubuser mshanemc --repo volvo-base
 sfdx shane:user:password:set -g User -l User -p DF18*cg1
 sfdx force:user:permset:assign -n VehiclesPerms
