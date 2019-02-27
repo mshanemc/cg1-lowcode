fd5829ecb649f998bf2d0751e4d6394dba5fa0e6
mshanemc <shane.mclaughlin@salesforce.com>
2019-02-27 17:24:14 -0600

    remove unused branding bits from base package, theme auto-activation

--- orgInit.sh
+++ orgInit.sh
@@ -4,4 +4,6 @@ sfdx shane:user:password:set -g User -l User -p DF18*cg1
 sfdx force:user:permset:assign -n VehiclesPerms
 sfdx force:source:push
 sfdx force:apex:execute -f scripts/init.cls
+sfdx shane:org:reauth -r --json
+sfdx shane:theme:activate -n Volvo
 sfdx force:org:open -p lightning/setup/ThemingAndBranding/home
