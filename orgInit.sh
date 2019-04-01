9fb811bad49939330af43c5d3e8a64529b182f5f
mshanemc <shane.mclaughlin@salesforce.com>
2019-04-01 14:58:00 -0500

    lowcode has reauth at least once

--- orgInit.sh
+++ orgInit.sh
@@ -3,6 +3,7 @@ sfdx shane:github:package:install --githubuser mshanemc --repo volvo-base
 sfdx force:user:permset:assign -n VehiclesPerms
 sfdx force:source:push
 sfdx force:apex:execute -f scripts/init.cls
+sfdx shane:org:reauth -r --json
 sfdx shane:theme:activate -n Volvo
 sfdx force:org:open -p lightning/setup/ThemingAndBranding/home
 sfdx shane:user:password:set -g User -l User -p DF18*cg1
